95695457dc89163b2405f64fbc1e0ee45aecc48b
Amrit Raj <98879654+Amritraj2008@users.noreply.github.com>
2022-06-07 20:50:56 +0530

    Update __init__.py

--- Harry/__init__.py
+++ Harry/__init__.py
@@ -42,7 +42,7 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-LOGGER.info("[Jarry] Starting Harry...")
+LOGGER.info("[Harry] Starting Harry...")
 
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
