be237dbd2d79857864a3ba1bc4befe42ef215910
Amrit Raj <98879654+Amritraj2008@users.noreply.github.com>
2022-06-08 11:58:23 +0530

    Update disable_sql.py

--- Harry/modules/sql/disable_sql.py
+++ Harry/modules/sql/disable_sql.py
@@ -20,7 +20,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, func, distinct
 
-from zeldris.modules.sql import SESSION, BASE
+from Harry.modules.sql import SESSION, BASE
 
 
 class Disable(BASE):
