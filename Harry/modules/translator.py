beb7313b8f2f6cfb68c17a7b2b3d956e952f18e1
Amrit Raj <98879654+Amritraj2008@users.noreply.github.com>
2022-06-08 08:29:59 +0530

    Update translator.py

--- Harry/modules/translator.py
+++ Harry/modules/translator.py
@@ -31,9 +31,9 @@ from telegram import (
 )
 from telegram.ext import CallbackContext
 
-from zeldris import dispatcher
-from zeldris.modules.disable import DisableAbleCommandHandler
-from zeldris.modules.helper_funcs.alternate import typing_action, send_action
+from Harry import dispatcher
+from Harry.modules.disable import DisableAbleCommandHandler
+from Harry.modules.helper_funcs.alternate import typing_action, send_action
 
 trans = SyncTranslator()
 
