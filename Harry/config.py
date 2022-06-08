e01078d738a38dd230a25aac75c678a24bfcbf7c
Amrit Raj <98879654+Amritraj2008@users.noreply.github.com>
2022-06-08 15:34:38 +0530

    Update config.py

--- Harry/config.py
+++ Harry/config.py
@@ -1,34 +1,4 @@
-# ZeldrisRobot
-# Copyright (C) 2017-2019, Paul Larsen
-# Copyright (C) 2022, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-
-if not __name__.endswith("sample_config"):
-    import sys
-
-    print(
-        "The README is there to be read. Extend this sample config to a config file, don't just rename and change "
-        "values here. Doing that WILL backfire on you.\nBot quitting.",
-        file=sys.stderr,
-    )
-    sys.exit(1)
-
-
-# Create a new config.py file in same dir and import, then extend this class.
-class Config(object):
+ Config(object):
     LOGGER = True
 
     # REQUIRED
