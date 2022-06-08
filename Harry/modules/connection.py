adbf641d48492e75f824717dbab84da6cd879dab
Amrit Raj <98879654+Amritraj2008@users.noreply.github.com>
2022-06-08 08:56:49 +0530

    Update connection.py

--- Harry/modules/connection.py
+++ Harry/modules/connection.py
@@ -24,10 +24,10 @@ from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Upda
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, CallbackQueryHandler, CallbackContext
 
-import zeldris.modules.sql.connection_sql as sql
-from zeldris import dispatcher, DEV_USERS
-from zeldris.modules.helper_funcs import chat_status
-from zeldris.modules.helper_funcs.alternate import send_message, typing_action
+import Harry.modules.sql.connection_sql as sql
+from Harry import dispatcher, DEV_USERS
+from Harry.modules.helper_funcs import chat_status
+from Harry.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
