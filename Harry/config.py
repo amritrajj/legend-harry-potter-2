058ae0f77368acea92873c0c7f6e1a742d29f619
Amrit Raj <98879654+Amritraj2008@users.noreply.github.com>
2022-06-09 06:10:12 +0530

    Update config.py

--- Harry/config.py
+++ Harry/config.py
@@ -1,56 +1,75 @@
- Config(object):
-    LOGGER = True
+# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
+import json
+import os
 
+
+def get_user_list(config, key):
+    with open("{}/GroupMenter/{}".format(os.getcwd(), config), "r") as json_file:
+        return json.load(json_file)[key]
+
+
+# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
+class Config(object):
+    LOGGER = True
     # REQUIRED
-    TOKEN = ""  # Take from @BotFather
-    OWNER_ID = (
-        ""  # If you dont know, run the bot and do /id in your private chat with it
-    )
-    OWNER_USERNAME = ""
-    API_HASH = None  # for purge stuffs
-    API_ID = None
+    # Login to https://my.telegram.org and fill in these slots with the details given by it
+
+    API_ID = 4951377  # integer value, dont use ""
+    API_HASH = "b682525d7bd7be0b7f61502cc1c8b22e"
+    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1152303959  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "amritraj20_08"
+    SUPPORT_CHAT = "tech_sav_bot"  # Your own group for support, do not add the @
+    JOIN_LOGGER = (
+        -1001777503234
+    )  # Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = (
+        -1001794293836
+    )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "sqldbtype://username:pw@hostname:port/db_name"  # needed for any database modules
-    MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
-    REDIS_URL = "redis://something@nothing/anything:10002"  # needed for afk module, get from redislab
+    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
     LOAD = []
-    NO_LOAD = []
+    NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
+    INFOPIC = True
     URL = None
-    MONGO_URI = ""
-    MONGO_PORT = 27017  # leave it as it is
-    MONGO_DB = "Harry"
+    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     # OPTIONAL
-    DEV_USERS = (
-        []
-    )  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = (
-        []
-    )  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = (
-        []
-    )  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    WHITELIST_CHATS = []
-    BLACKLIST_CHATS = []
+    ##List of id's -  (not usernames) for users which have sudo access to the bot.
+    DRAGONS = get_user_list("elevated_users.json", "sudos")
+    ##List of id's - (not usernames) for developers who will have the same perms as the owner
+    DEV_USERS = get_user_list("elevated_users.json", "devs")
+    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    DEMONS = get_user_list("elevated_users.json", "supports")
+    # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    TIGERS = get_user_list("elevated_users.json", "tigers")
+    WOLVES = get_user_list("elevated_users.json", "whitelists")
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
     PORT = 5000
-    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
+    DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
     STRICT_GBAN = True
-    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = None  # banhammer marie sticker
-    ALLOW_EXCL = False  # DEPRECATED, USE BELOW INSTEAD! Allow ! commands as well as /
-    CUSTOM_CMD = False  # Set to ('/', '!') or whatever to enable it, like ALLOW_EXCL but with more custom handler!
-    API_OPENWEATHER = None  # OpenWeather API
-    SPAMWATCH_API = None  # Your SpamWatch token
-    WALL_API = None
+    WORKERS = (
+        8  # Number of subthreads to use. Set as number of threads your processor uses
+    )
+    BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
+    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
+    CASH_API_KEY = (
+        "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
+    )
+    TIME_API_KEY = "awoo"  # Get your API key from https://timezonedb.com/api
+    WALL_API = (
+        "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
+    )
+    AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
+    BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
 
-
 class Production(Config):
-    LOGGER = False
+    LOGGER = True
 
 
 class Development(Config):
